5476b8579703aca8a890e60b84d8c3e95b5c5a35
Bulat Gabidullin <bulat.gabidullin@viamo.io>
2023-01-26 23:24:59 +0100

    add firebase

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
+import {initializeApp} from 'firebase/app'
+import {getAnalytics} from 'firebase/analytics'
 import './style.css'
 import App from './App.vue'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA3LuZ-FllYGxn_-OPn_hSeEILRcrubayQ',
+  authDomain: 'asana-clone-c80fd.firebaseapp.com',
+  projectId: 'asana-clone-c80fd',
+  storageBucket: 'asana-clone-c80fd.appspot.com',
+  messagingSenderId: '497348060162',
+  appId: '1:497348060162:web:5fa774d6076a1148b7731b',
+  measurementId: 'G-8GZ6J6VBBJ'
+}
+const firebaseApp = initializeApp(firebaseConfig)
+const analytics = getAnalytics(firebaseApp)
 
 createApp(App).mount('#app')
